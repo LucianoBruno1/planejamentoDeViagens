43d2dfd5199afd3a402bf796464453a791679e8d
Luciano Bruno <luciaanobruno@gmail.com>
2024-08-06 22:01:25 -0300

    Login

--- planejamentoDeViagens/urls.py
+++ planejamentoDeViagens/urls.py
@@ -1,7 +1,11 @@
+# planejamentoDeViagens/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
+from planejamentoDeViagens.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('', home, name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-# users/views.py
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
@@ -19,3 +19,23 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redireciona para a página inicial após o login bem-sucedido
+        else:
+            return render(request, 'login.html', {'form': form, 'error': 'Invalid username or password'})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
